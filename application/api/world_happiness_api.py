3284b64244338b59c8be125b2a0328c33f78b2e9
Sean Wu <sean@seanwu.co>
2018-05-14 20:09:16 +1000

    Working data publication api with the World Happiness Report dataset (2015-2017). The architecture can be considered for integration of all APIs. Testing can be conducted using any REST API tester browser plugin.

---
+++ application/api/world_happiness_api.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.exceptions import BadRequest, NotFound
+from application.models import world_happiness_model
+
+mod = Blueprint('world_happiness', __name__)
+
+
+# Returns all 3 years data of all countries.
+@mod.route('/api/world_happiness', methods=['GET'])
+def get_all_data():
+    result = world_happiness_model.get_collection()
+    if result:
+        return jsonify(collection=result), 200
+    else:
+        raise NotFound
+
+
+# Returns 1 year data of all countries.
+# 2015 <= year <= 2017
+@mod.route('/api/world_happiness/<int:year>', methods=['GET'])
+def get_data_by_year(year):
+    result = world_happiness_model.get_entry_by_year(year)
+    if result:
+        return jsonify(result), 200
+    else:
+        raise NotFound
+
+
+# Returns all 3 years data of one country.
+@mod.route('/api/world_happiness/<country>', methods=['GET'])
+def get_data_by_country(country):
+    result = world_happiness_model.get_entries_by_country(country)
+    if result:
+        return jsonify(result), 200
+    else:
+        raise NotFound
+
+
+# Returns 1 year data of one country.
+# Request must provide filter (year and country) in URL query string
+@mod.route('/api/world_happiness/filter', methods=['GET'])
+def get_data_by_filter():
+    if 'year' not in request.args or 'country' not in request.args:
+        raise BadRequest
+    year = int(request.args['year'])
+    country = request.args['country']
+    result = world_happiness_model.get_entry_by_filter(year, country)
+    if result:
+        return jsonify(result), 200
+    else:
+        raise NotFound
+
+
+# Delete all data.
+# Not implemented in app GUI. Accessible through browser's REST API plugin.
+@mod.route('/api/world_happiness', methods=['DELETE'])
+def delete_all_data():
+    result = world_happiness_model.delete_collection()
+    if result:
+        return '', 200
+    else:
+        raise NotFound
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from application.api import world_happiness_api
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.register_blueprint(world_happiness_api.mod)
+
+
+if __name__ == '__main__':
+    server_port = 5000
+    max_retries = 20
+    for _ in range(max_retries):
+        try:
+            app.run(port=server_port)
+        except OSError as e:
+            if e.errno == 48:
+                server_port += 150
+            else:
+                raise e
+        else:
+            break
+    else:
+        raise OSError(('Server cannot be started after trying '
+                       '{} different ports.').format(max_retries))
