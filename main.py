6e740907c1cfef055592bb0e23799b8a4089d436
Sean Wu <sean@seanwu.co>
2018-05-14 23:43:12 +1000

    Added CORS support.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from application.api import world_happiness_api
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 app.register_blueprint(world_happiness_api.mod)
+CORS(app)
 
 
 if __name__ == '__main__':
