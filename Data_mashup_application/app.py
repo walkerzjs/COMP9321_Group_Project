f2f6ca8176049fa533af831fdce5a8561693a38e
Jacob Zhang <jacobjszha>
2018-05-26 01:36:20 +1000

    changed to ajax in correlation analysis view and modified some descriptions.

--- Data_mashup_application/app.py
+++ Data_mashup_application/app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 import os
 from flask_cors import CORS
 from correlation_analysis.view import correlation_analysis
+from correlation_analysis.api import correlation_analysis_api
 from similarity_analysis import similarity_analysis_view
 from similarity_analysis import similarity_analysis
 from happiness_ranking_by_country.api import happiness_ranking_api
@@ -17,6 +18,7 @@ app = Flask(__name__, template_folder = template_dir)
 
 app.config['JSON_SORT_KEYS'] = False
 app.register_blueprint(correlation_analysis.cor)
+app.register_blueprint(correlation_analysis_api.mod)
 app.register_blueprint(similarity_analysis.mod)
 app.register_blueprint(similarity_analysis_view.mod)
 app.register_blueprint(happiness_ranking_api.mod)
